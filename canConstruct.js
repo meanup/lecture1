9a689ea85f64c9580616fad41beb319e81d92f47
meanup <71303572+meanup@users.noreply.github.com>
2021-05-02 20:10:24 +0530

    Update canConstruct.js

--- canConstruct.js
+++ canConstruct.js
@@ -1,21 +1,21 @@
-const canConstruct = (target, wordBank) => {
-    if (target === '') {
-        return true;
-    }
+const canConstruct = (target, wordBank, memo = {}) => {
+    if (target in memo) return memo[target];
+    if (target === '') return true;
 
     for (let word of wordBank){
         if (target.indexOf(word) === 0){
             const suffix = target.slice(word.length);
-            if (canConstruct(suffix, wordBank) === true){
+            if (canConstruct(suffix, wordBank, memo) === true){
+                memo[target] = true;
                 return true;
             }
         }
     }
-
-    return false;
+memo[target] = false;
+return false;
 }
 
 console.log(canConstruct("abcdef", ["ab", "abc","cd", "def", "abcd"]));
 console.log(canConstruct("skateboard" , ["bo", "rd", "ate", "t", "ska", "sk", "boar"]));
 console.log(canConstruct("enterapotentpot" , ["a", "p", "ent", "enter", "ot", "o", "t"]));
-console.log(canConstruct("eeeeeeeeeeeeeeef",["e", "ee","eee", "eeee"]));
+console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",["e", "ee","eee", "eeee", "eeeee", "eeeeee"]));
