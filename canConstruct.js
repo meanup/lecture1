b104e1928b6c5c66faf72eb8fddb437ecc7379e1
meanup <71303572+meanup@users.noreply.github.com>
2021-05-02 19:12:10 +0530

    Update canConstruct.js

--- canConstruct.js
+++ canConstruct.js
@@ -16,3 +16,6 @@ const canConstruct = (target, wordBank) => {
 }
 
 console.log(canConstruct("abcdef", ["ab", "abc","cd", "def", "abcd"]));
+console.log(canConstruct("skateboard" , ["bo", "rd", "ate", "t", "ska", "sk", "boar"]));
+console.log(canConstruct("enterapotentpot" , ["a", "p", "ent", "enter", "ot", "o", "t"]));
+console.log(canConstruct("eeeeeeeeeeeeeeef",["e", "ee","eee", "eeee"]));
