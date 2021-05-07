168be78b6a292857dd487fc4c1a3cd9e85d2951d
meanup <71303572+meanup@users.noreply.github.com>
2021-05-07 12:12:50 +0530

    Create allConstruct.js

---
+++ allConstruct.js
@@ -0,0 +1,26 @@
+const allConstruct = (target, wordBank) => {
+    if (target === '' ) return [[]];
+
+    const result = [];
+
+    for (let word of wordBank){
+        if (target.indexOf(word) === 0){
+            const suffix = target.slice(word.length);
+            const suffixWays = allConstruct(suffix, wordBank);
+            const targetWays = suffixWays.map(way => [word, ...way]);
+            result.push(...targetWays);
+        }
+    }
+    return result;
+}
+
+console.log(allConstruct('purple',['purp','p','ur','le','purpl'])); //[ [ 'purp', 'le' ], [ 'p', 'ur', 'p', 'le' ] ]
+console.log(allConstruct('abcdef',['ab','abc','cd','def','abcd','ef','c']));
+//[
+//  [ 'ab', 'cd', 'ef' ],
+//  [ 'ab', 'c', 'def' ],
+//  [ 'abc', 'def' ],
+//  [ 'abcd', 'ef' ]
+//]
+console.log(allConstruct("skateboard" , ["bo", "rd", "ate", "t", "ska", "sk", "boar"])); //[]
+console.log(allConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",["e", "ee","eee", "eeee", "eeeee", "eeeeee"])); //time complexity
