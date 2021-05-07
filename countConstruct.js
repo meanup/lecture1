d8af562297b7977dde3fe04db88eb662f0188003
meanup <71303572+meanup@users.noreply.github.com>
2021-05-07 11:04:01 +0530

    Update countConstruct.js

--- countConstruct.js
+++ countConstruct.js
@@ -1,1 +1,21 @@
+const countConstruct = (target, wordBank, memo = {}) => {
+    if (target in memo) return memo[target];
+    if (target === '') return 1;
 
+    let totalCount = 0;
+
+    for (let word of wordBank) {
+        if (target.indexOf(word) === 0){
+            const numWaysForRest = countConstruct(target.slice(word.length), wordBank, memo);
+            totalCount += numWaysForRest;
+        }
+    }
+    memo[target] = totalCount;
+    return totalCount;
+}
+
+console.log(countConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl' ]));
+console.log(countConstruct("abcdef", ["ab", "abc","cd", "def", "abcd"]));
+console.log(countConstruct("skateboard" , ["bo", "rd", "ate", "t", "ska", "sk", "boar"]));
+console.log(countConstruct("enterapotentpot" , ["a", "p", "ent", "enter", "ot", "o", "t"]));
+console.log(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",["e", "ee","eee", "eeee", "eeeee", "eeeeee"]));
