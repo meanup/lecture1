907ef97d4109dac915f0f1b4fdd2eaa669561ca0
meanup <71303572+meanup@users.noreply.github.com>
2021-05-10 21:21:42 +0530

    Update howSum-tabulation.js

--- howSum-tabulation.js
+++ howSum-tabulation.js
@@ -1,20 +1,21 @@
 const howSum = (targetSum, numbers) => {
   
-  const tavle = Array(targetSum + 1).fill(null);
-  table[0] = [];
-  
-  for (let i = 0; i <= targetSum; i++){ // m times
-    if (table[i] !== null){
-      for (let num of numbers){ // n times
-        table[i + num] = [...table[i], num];
+    const table = Array(targetSum + 1).fill(null);
+    table[0] = [];
+    
+    for (let i = 0; i <= targetSum; i++){ // m times
+      if (table[i] !== null){
+        for (let num of numbers){ // n times
+          table[i + num] = [...table[i], num];
+        }
       }
     }
+    return table[targetSum];
   }
-  return table[targetSum];
-}
-
-console.log(canSum(7, [2, 3])); //[3, 2, 2]
-console.log(canSum(7, [5, 3, 4, 7])); //[4,3]
-console.log(canSum(7, [2, 4])); //null
-console.log(canSum(8, [2, 3, 5])); // [2, 2, 2, 2]
-console.log(canSum(300, [7, 14])); // null
+  
+  console.log(howSum(7, [2, 3])); //[3, 2, 2]
+  console.log(howSum(7, [5, 3, 4, 7])); //[4,3]
+  console.log(howSum(7, [2, 4])); //null
+  console.log(howSum(8, [2, 3, 5])); // [2, 2, 2, 2]
+  console.log(howSum(300, [7, 14])); // null
+  
