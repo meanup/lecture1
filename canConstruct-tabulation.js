24629b0ebce3c4dc95664fd8ab66ad6be693a3d3
meanup <71303572+meanup@users.noreply.github.com>
2021-05-10 21:11:19 +0530

    Create canConstruct-tabulation.js

---
+++ canConstruct-tabulation.js
@@ -0,0 +1,18 @@
+const canConstruct = (target, wordBank) => {
+  const table = Array(targetSum + 1).fill(false);
+  table[0] = true;
+  
+  for (let i = 0; i <= taregt.length; i++){
+    if (table[i] === true) {
+      for (let word of wordBank){
+        if (target.slice(i, i + word.length) === word){
+            table[i + word.length] = true;
+        }
+      }
+    }
+  }
+  return table[target.length];
+  }
+
+console.log(canConstruct("abcdef", ["ab", " abc", "cd", "def", "abcd"])); // true
+console.log(canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])); // false
