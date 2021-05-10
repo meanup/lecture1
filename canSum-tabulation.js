64a624861fe719a22511024e1a16f7b064782655
meanup <71303572+meanup@users.noreply.github.com>
2021-05-10 19:59:22 +0530

    Update canSum-tabulation.js

--- canSum-tabulation.js
+++ canSum-tabulation.js
@@ -6,7 +6,7 @@
 const canSum = (targetSum, numbers) => {
   const table = Array(tagertSum + 1).fill(false);
   table[0] = true;
-  for (let i = 0; i <= table.length; i++){
+  for (let i = 0; i <= targetSum; i++){ //table.length
     if (table[i] === true) {
       for (let num of numbers){
         table[i + num] = true;
@@ -19,5 +19,10 @@ const canSum = (targetSum, numbers) => {
 console.log(canSum(7, [2, 3])); //true
 console.log(canSum(7, [5, 3, 4, 7])); //true
 console.log(canSum(7, [2, 4])); //false
-console.log(CanSum(8, [2, 3, 5])); // true
+console.log(canSum(8, [2, 3, 5])); // true
 console.log(canSum(300, [7, 14])); // false
+
+// Testing 
+// const arr = ['a', 'b', 'c'];
+// arr[10] = 'x';
+// console.log(arr);
