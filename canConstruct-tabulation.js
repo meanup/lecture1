ad0f9a2116f55d66d4e2386b9a1417927f3a823e
meanup <71303572+meanup@users.noreply.github.com>
2021-05-10 21:12:50 +0530

    Update canConstruct-tabulation.js

--- canConstruct-tabulation.js
+++ canConstruct-tabulation.js
@@ -16,3 +16,5 @@ const canConstruct = (target, wordBank) => {
 
 console.log(canConstruct("abcdef", ["ab", " abc", "cd", "def", "abcd"])); // true
 console.log(canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])); // false
+console.log(canConstruct("enterapotentpot" , ["a", "p", "ent", "enter", "ot", "o", "t"]));
+console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",["e", "ee","eee", "eeee", "eeeee", "eeeeee"]));
