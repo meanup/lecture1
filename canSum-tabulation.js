9a2bc1c53b1f242799fced6926948b0799c4f317
meanup <71303572+meanup@users.noreply.github.com>
2021-05-10 19:56:33 +0530

    Create canSum-tabulation.js

---
+++ canSum-tabulation.js
@@ -0,0 +1,23 @@
+// canSum(7, [5, 3, 4]) -> true
+// length = targetSum + 1
+// Boolean Data start with F value
+// canSum(0, [...]) -> true
+
+const canSum = (targetSum, numbers) => {
+  const table = Array(tagertSum + 1).fill(false);
+  table[0] = true;
+  for (let i = 0; i <= table.length; i++){
+    if (table[i] === true) {
+      for (let num of numbers){
+        table[i + num] = true;
+      }
+    }
+  }
+  return table[targetSum];
+}
+
+console.log(canSum(7, [2, 3])); //true
+console.log(canSum(7, [5, 3, 4, 7])); //true
+console.log(canSum(7, [2, 4])); //false
+console.log(CanSum(8, [2, 3, 5])); // true
+console.log(canSum(300, [7, 14])); // false
