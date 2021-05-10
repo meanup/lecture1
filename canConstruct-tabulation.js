bdbefdfd39f623684cb9af0b5bc43f8fa8cfcf3b
meanup <71303572+meanup@users.noreply.github.com>
2021-05-10 21:14:45 +0530

    Update canConstruct-tabulation.js

--- canConstruct-tabulation.js
+++ canConstruct-tabulation.js
@@ -1,20 +1,20 @@
 const canConstruct = (target, wordBank) => {
-  const table = Array(targetSum + 1).fill(false);
-  table[0] = true;
-  
-  for (let i = 0; i <= taregt.length; i++){
-    if (table[i] === true) {
-      for (let word of wordBank){
-        if (target.slice(i, i + word.length) === word){
-            table[i + word.length] = true;
+    const table = Array(target.length + 1).fill(false);
+    table[0] = true;
+    
+    for (let i = 0; i <= target.length; i++){
+      if (table[i] === true) {
+        for (let word of wordBank){
+          if (target.slice(i, i + word.length) === word){
+              table[i + word.length] = true;
+          }
         }
       }
     }
-  }
-  return table[target.length];
-  }
-
-console.log(canConstruct("abcdef", ["ab", " abc", "cd", "def", "abcd"])); // true
-console.log(canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])); // false
-console.log(canConstruct("enterapotentpot" , ["a", "p", "ent", "enter", "ot", "o", "t"]));
-console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",["e", "ee","eee", "eeee", "eeeee", "eeeeee"]));
+    return table[target.length];
+    }
+  
+  console.log(canConstruct("abcdef", ["ab", " abc", "cd", "def", "abcd"])); // true
+  console.log(canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])); // false
+  console.log(canConstruct("enterapotentpot" , ["a", "p", "ent", "enter", "ot", "o", "t"]));
+  console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",["e", "ee","eee", "eeee", "eeeee", "eeeeee"]));
